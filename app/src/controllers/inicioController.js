c2639f9871ce75915f3ec43383e4b4851fc5f93b
Wbeimar <wlogatto@icloud.com>
2018-03-25 00:40:58 -0500

    modal show comic, lateral bar favourities, service get comic

--- app/src/controllers/inicioController.js
+++ app/src/controllers/inicioController.js
@@ -4,12 +4,23 @@ spaApp
   .controller('InicioCtrl', function(inicioService) {
 
     this.listCharacters = [];
+    this.modalComic = {};
 
     this.search = () =>{
         console.log("SEARch",this.word);
         inicioService.getCharacters(this.word).then( (result) =>{
-            console.log("TERMINO",result);
-            this.listCharacters = result.data.results;
+          console.log("TERMINO",result);
+          this.listCharacters = result.data.results;
         });
     }
+
+    this.getComic = (comic) =>{
+      inicioService.getComic(comic).then( (result)=>{
+        console.log("RESU",result.data.results);
+        this.modalComic = result.data.results[0];
+      });
+    }
+
+
+
   } );
--- app/src/services/inicioService.js
+++ app/src/services/inicioService.js
@@ -1,7 +1,7 @@
 var PRIV_KEY = "03656eeb2065730d32c6cf2ef11e50f4fb91986e";
 var PUBLIC_KEY = "fa2de1079f8bd81b19259c02befff1f0";
 
-var URLBASE = "https://gateway.marvel.com:443/v1/public/characters";
+var URLBASE = "https://gateway.marvel.com:443/v1/public";
 
 spaApp.factory('inicioService', function($http, $log,md5){
     return {
@@ -18,11 +18,28 @@ spaApp.factory('inicioService', function($http, $log,md5){
                 limit:10
             }
 
-            return $http({method: 'GET', url:URLBASE, params:data_ }).
+            return $http({method: 'GET', url:URLBASE+'/characters', params:data_ }).
                 then(function(response){
                     return response.data;
                 });     
+        },
+        getComic:function(comic){
+            var ts = new Date().getTime();
+            var hash = md5.createHash( ts + PRIV_KEY + PUBLIC_KEY);
+
+            data_ = {
+                ts: ts,
+                apikey: PUBLIC_KEY,
+                hash: hash
+            }
+
+            return $http({method: 'GET', url: comic.resourceURI  , params:data_ }).
+                then(function(response){
+                    return response.data;
+                }); 
+
         }
+
     };	
 }
 );
